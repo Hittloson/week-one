aac25abc0d550dc91f094891bdcf5e5062492226
Hittloson <hittlo86@gmail.com>
2023-07-28 22:50:51 +0000

    updates

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ContentStyle from './ContentStyle.css';
 
 
+
 function content() {
   return (
-    <div>content</div>
+    <main className='main-content'>
+      <div className='content'>
+        <div className='content-text'>
+          <h3>Text</h3>
+        </div>
+        <div className='content-img'>
+          <img src='image/img-1.jpg'/>
+        </div>
+      </div>
+    </main>
   )
 }
 
