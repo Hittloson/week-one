7fd5d38ad22fdee31d624373b38abae79c3d4240
Hittloson <hittlo86@gmail.com>
2023-07-29 00:14:06 +0000

    update

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,10 +8,39 @@ function content() {
     <main className='main-content'>
       <div className='content'>
         <div className='content-text'>
-          <h3>Text</h3>
+          <h3><span>Welcome</span> to our store</h3>
+          <p>Our website is your perfect destination to find the 
+          perfect fragrance that reflect your personality and 
+          enhances your elegance you can choose the scent 
+          that aligns with your lifestyle</p>
         </div>
         <div className='content-img'>
-          <img src='image/img-1.jpg'/>
+          <img src='image/img5.jpg'/>
+        </div>
+      </div>
+      <div className='content'>
+        
+        <div className='content-img'>
+          <img src='image/header-bg4.jpg'/>
+        </div>
+        <div className='content-text'>
+          <h3><span>Welcome</span> to our store</h3>
+          <p>Our website is your perfect destination to find the 
+          perfect fragrance that reflect your personality and 
+          enhances your elegance you can choose the scent 
+          that aligns with your lifestyle</p>
+        </div>
+      </div>
+      <div className='content'>
+        <div className='content-text'>
+          <h3><span>Welcome</span> to our store</h3>
+          <p>Our website is your perfect destination to find the 
+          perfect fragrance that reflect your personality and 
+          enhances your elegance you can choose the scent 
+          that aligns with your lifestyle</p>
+        </div>
+        <div className='content-img'>
+          <img src='image/img-group2.jpg'/>
         </div>
       </div>
     </main>
