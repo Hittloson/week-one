7c15f240ba441363bbdff46803d7a1b51bfa9d23
Hittloson <hittlo86@gmail.com>
2023-08-02 05:10:52 +0000

    update

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,44 +1,50 @@
-import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX, faPlus } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 
 function Questions() {
+  const [questions, setQuestions] = useState([
+    { question: "Hi Am Looking For A Product But Can't Find It Listed On Your Website, Where Is It?", answer: "Reference site about Lorem Ipsum,\ngiving information on its origins,\nas well as a random Lipsum generator.", isExpanded: false },
+    { question: "How Can I Cancel My Order?", answer: "Reference site about Lorem Ipsum,\ngiving information on its origins,\nas well as a random Lipsum generator.", isExpanded: false },
+    { question: "My Order Arrived Damaged", answer: "Reference site about Lorem Ipsum,\ngiving information on its origins,\nas well as a random Lipsum generator.", isExpanded: false },
+    { question: "How Do I Change My Address?", answer: "Reference site about Lorem Ipsum,\ngiving information on its origins,\nas well as a random Lipsum generator.", isExpanded: false }
+  ]);
+
+  const toggleQuestion = (index) => {
+    const newQuestions = [...questions];
+    newQuestions[index].isExpanded = !newQuestions[index].isExpanded;
+    setQuestions(newQuestions);
+  };
+
   return (
+    <>
     <div className='FAQ'>
-        <div>
-            <h1>FAQs Frequently Asked Questions</h1>
-            <p>Reference site about Lorem Ipsum, 
-            giving information on its origins, 
-            as well as a random Lipsum generator.</p>
-        </div> 
-        <div className='.question-answer'>
-                <div className='FAQ-content'>
-                    <h1>Hi Am Looking For A Product But Cant 
-                    Find It Listed On Your Website, Where Is It?</h1>
-                    <p>Reference site about Lorem Ipsum, 
-                    giving information on its origins, 
-                    as well as a random Lipsum generator.</p>
-                </div> 
-                <div className='FAQ-content'>
-                    <h1>How Can I Cancel My Order?</h1>
-                    <p>Reference site about Lorem Ipsum, 
-                    giving information on its origins, 
-                    as well as a random Lipsum generator.</p>
-                </div> 
-                <div className='FAQ-content'>
-                    <h1>My Order Arrived Damaged</h1>
-                    <p>Reference site about Lorem Ipsum, 
-                    giving information on its origins, 
-                    as well as a random Lipsum generator.</p>
-                </div> 
-                <div className='FAQ-content'>
-                    <h1>How Do I Change My Address?</h1>
-                    <p>Reference site about Lorem Ipsum, 
-                    giving information on its origins, 
-                    as well as a random Lipsum generator.</p>
-                </div> 
+      <div>
+        <h1>FAQs Frequently Asked Questions</h1>
+        <p>Reference site about Lorem Ipsum, 
+        giving information on its origins, 
+        as well as a random Lipsum generator.</p>
+      </div> 
+      <div className='question-answer'>
+        {questions.map((q, index) => (
+            <div className={`FAQ-content ${q.isExpanded ? '' : 'hidden'}`} key={index}>
+            <div className='icons'>
+              <FontAwesomeIcon icon={q.isExpanded ? faX : faPlus} onClick={() => toggleQuestion(index)} />
             </div>
+            <h1>{q.question}</h1>
+            <p style={{ display: q.isExpanded ? 'block' : 'none' }}>{q.answer}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+
+    <div>
+            <h1>Hello</h1>
+    </div>
+    </>
+   
+  );
+  
 }
 
-export default Questions
+export default Questions;
