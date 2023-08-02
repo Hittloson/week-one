99e1c86afb2f046f8771882451da284ef391f461
Hittloson <hittlo86@gmail.com>
2023-08-02 03:11:06 +0000

    update

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,43 +4,38 @@ import React from 'react'
 function Questions() {
   return (
     <div className='FAQ'>
-        <h1>FAQ's Frequently Asked Questions</h1>
-        <p>Reference site about Lorem Ipsum, 
-        giving information on its origins, 
-        as well as a random Lipsum generator.</p>
-
-        <div className='question-answer'>
-            <h1>Am Looking For A Product But Cant 
-            Find It Listed In Your Website, Where is it?</h1>
-            <p>Reference site about Lorem Ipsum, 
-        giving information on its origins, 
-        as well as a random Lipsum generator.</p>
-        
-        </div>
         <div>
-            <h1>Am Looking For A Product But Cant 
-            Find It Listed In Your Website, Where is it?</h1>
+            <h1>FAQs Frequently Asked Questions</h1>
             <p>Reference site about Lorem Ipsum, 
-        giving information on its origins, 
-        as well as a random Lipsum generator.</p>
-        
-        </div>
-        <div>
-            <h1>Am Looking For A Product But Cant 
-            Find It Listed In Your Website, Where is it?</h1>
-            <p>Reference site about Lorem Ipsum, 
-        giving information on its origins, 
-        as well as a random Lipsum generator.</p>
-        
-        </div>
-        <div>
-            <h1>Am Looking For A Product But Can't 
-            Find It Listed In Your Website, Where is it?</h1>
-            <p>Reference site about Lorem Ipsum, 
-        giving information on its origins, 
-        as well as a random Lipsum generator.</p>
-        
-        </div>
+            giving information on its origins, 
+            as well as a random Lipsum generator.</p>
+        </div> 
+        <div className='.question-answer'>
+                <div className='FAQ-content'>
+                    <h1>FAQs Frequently Asked Questions</h1>
+                    <p>Reference site about Lorem Ipsum, 
+                    giving information on its origins, 
+                    as well as a random Lipsum generator.</p>
+                </div> 
+                <div>
+                    <h1>FAQs Frequently Asked Questions</h1>
+                    <p>Reference site about Lorem Ipsum, 
+                    giving information on its origins, 
+                    as well as a random Lipsum generator.</p>
+                </div> 
+                <div>
+                    <h1>FAQs Frequently Asked Questions</h1>
+                    <p>Reference site about Lorem Ipsum, 
+                    giving information on its origins, 
+                    as well as a random Lipsum generator.</p>
+                </div> 
+                <div>
+                    <h1>FAQs Frequently Asked Questions</h1>
+                    <p>Reference site about Lorem Ipsum, 
+                    giving information on its origins, 
+                    as well as a random Lipsum generator.</p>
+                </div> 
+            </div>
     </div>
   )
 }
