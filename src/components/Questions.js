9bde5863d21d3a6f45770ca43972a59a3eb08791
Hittloson <hittlo86@gmail.com>
2023-08-02 03:35:23 +0000

    update

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -12,25 +12,26 @@ function Questions() {
         </div> 
         <div className='.question-answer'>
                 <div className='FAQ-content'>
-                    <h1>FAQs Frequently Asked Questions</h1>
+                    <h1>Hi Am Looking For A Product But Cant 
+                    Find It Listed On Your Website, Where Is It?</h1>
                     <p>Reference site about Lorem Ipsum, 
                     giving information on its origins, 
                     as well as a random Lipsum generator.</p>
                 </div> 
-                <div>
-                    <h1>FAQs Frequently Asked Questions</h1>
+                <div className='FAQ-content'>
+                    <h1>How Can I Cancel My Order?</h1>
                     <p>Reference site about Lorem Ipsum, 
                     giving information on its origins, 
                     as well as a random Lipsum generator.</p>
                 </div> 
-                <div>
-                    <h1>FAQs Frequently Asked Questions</h1>
+                <div className='FAQ-content'>
+                    <h1>My Order Arrived Damaged</h1>
                     <p>Reference site about Lorem Ipsum, 
                     giving information on its origins, 
                     as well as a random Lipsum generator.</p>
                 </div> 
-                <div>
-                    <h1>FAQs Frequently Asked Questions</h1>
+                <div className='FAQ-content'>
+                    <h1>How Do I Change My Address?</h1>
                     <p>Reference site about Lorem Ipsum, 
                     giving information on its origins, 
                     as well as a random Lipsum generator.</p>
